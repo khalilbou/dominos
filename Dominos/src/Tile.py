f7c2deb4d2e021a49579c71ce558299634c0bf70
didooofidooo <didooofidooo@8686c424-25b9-11de-b186-ef1da62b6547>
2009-04-16 21:56:41 +0000

    Initial import.

---
+++ Dominos/src/Tile.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys, pygame
+
+class Tile(object):
+	def __init__(self, number1, number2, screen, X, Y):
+		
+		self._number1 = number1
+		self._number2 = number2
+		
+		self._X = X
+		self._Y = Y
+		
+		self._screen = screen
+		
+		image1_place = "images/" + str(self._number1) + ".png"
+		self._square1 = pygame.image.load(image1_place)
+		
+		image2_place = "images/" + str(self._number2) + ".png"
+		self._square2 = pygame.image.load(image2_place)
+
+	def show_horizontal(self):
+		self._screen.blit(self._square1, (self._X,self._Y))
+		self._screen.blit(self._square2, ((self._X+34),self._Y))
+		pygame.display.flip()
+
+
+	def show_vertical(self):
+		self._screen.blit(self._square1, (self._X, self._Y))
+		self._screen.blit(self._square2, (self._X, (self._Y+34)))
+		pygame.display.flip()
---
+++ Dominos/src/computer.py
@@ -0,0 +1,150 @@
+'''
+Created on Apr 7, 2009
+
+@author: Mohamed Emam
+'''
+
+class computer(object):
+    '''
+    This Class represents the computer, including all
+    functions responsible for AI decisions
+    '''
+
+
+    def __init__(self, computer_tiles_list):
+        '''
+        The Constructor takes a list of tuples that
+        represents the computer tiles set
+        '''
+        self._tiles_list=computer_tiles_list
+        
+#---------------------------------------------------
+    
+    def play(self, played_tiles):
+        """
+        play(played_tiles)
+            returns the best tile to play. or returns "PASS" if
+            there are no suitable tiles.
+        """
+        suitable_tiles=self.get_suitable_tiles(played_tiles[0][0], played_tiles[-1][1])
+        
+        #there is no suitable tiles, return "PASS"
+        if len(suitable_tiles) == 0 :
+            return "PASS"
+        
+        #there is only one suitable tile, return it
+        elif len(suitable_tiles) == 1 :
+            self._tiles_list.remove(suitable_tiles[0])
+            return suitable_tiles[0]
+        
+        #there are more than one suitable tile, ask the "get_best_tile" method
+        else :
+            best_tile=self.get_best_tile(played_tiles, suitable_tiles)
+            self._tiles_list.remove(best_tile)
+            return best_tile
+
+#---------------------------------------------------
+    
+    def get_suitable_tiles(self, left_value, right_value):
+        """
+        get_suitable_tiles(left_value, right_value)
+            returns a list of tuples that contains the suitable tiles
+            to play.
+            if there are no suitable tiles, returns an empty list.
+        """
+        suitable_tiles=[]
+        
+        for tile in self._tiles_list :
+            if (left_value in tile) or (right_value in tile) :
+                suitable_tiles.append(tile)
+                
+        return suitable_tiles
+
+#---------------------------------------------------
+    
+    def get_best_tile(self, played_tiles, suitable_tiles):
+        
+        priority_list=[]
+        left_value = played_tiles[0][0]
+        right_value = played_tiles[-1][1]
+        
+        #build the priority_list
+        priority_list = range(len(suitable_tiles))
+        
+        #fill the priority_list
+        for j in range(len(suitable_tiles)) :
+            
+            dot_count=suitable_tiles[j][0] + suitable_tiles[j][1]
+            DUO = 0
+            
+            #check for the first condition (Total number is Large)
+            if dot_count > 7:
+                priority_list[j] += (dot_count - 7) + 3
+            
+            #check for the second condition (the tile is DUO
+            #and I can play on both sides)
+            if suitable_tiles[j][0] == suitable_tiles[j][1]:
+                
+                DUO = 1
+                
+                left_value_check = 0
+                right_value_check = 0
+                
+                for tile in suitable_tiles :
+                    if left_value in tile :
+                        left_value_check = 1
+                    if right_value in tile :
+                        right_value_check = 1
+                
+                if (left_value_check == 1) and (right_value_check == 1):
+                    priority_list[j] += 5
+                else :
+                    priority_list[j] += 2
+            
+            #check for the third condition, "Keep The Game Open"
+            #only in the begining of the game
+            #NEEDS TO BE TUNED FOR THE NUMBER OF PLAYERS
+            if len(self._tiles_list) > 4 :
+                x,y = suitable_tiles[j]
+                
+                x_count = 0
+                y_count = 0
+                
+                for tile in self._tiles_list :
+                    if x in tile :
+                        x_count += 1
+                    if y in tile :
+                        y_count += 1
+                
+                if (x_count == 1) or (y_count == 1):
+                    priority_list[j] -= 3
+            
+            #Check for the fourth condition, Watch out if the
+            #tile is played 6 times
+            x,y = suitable_tiles[j]
+                
+            x_count = 0
+            y_count = 0
+            
+            for tile in played_tiles :
+                if x in tile :
+                    x_count += 1
+                if y in tile :
+                    y_count += 1
+            
+            if (x_count == 6) or (y_count == 6):
+                priority_list[j] -= 10
+        
+        #Check for the fifth condition, the Biggest tile
+        #gets extra 2 points
+        sum_list=[]
+        for k,v in suitable_tiles :
+            sum_list.append(k+v)
+            
+        max_tile = sum_list.index(max(sum_list))
+        priority_list[max_tile] += 2
+        
+        #FINALLY, return the best tile
+        best_tile_index = priority_list.index(max(priority_list))
+        return suitable_tiles[best_tile_index]
+
