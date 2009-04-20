3983f7798c23924d3e36a3c1bf5f28f23263a4cf
BooDy.FCI <BooDy.FCI@8686c424-25b9-11de-b186-ef1da62b6547>
2009-04-20 16:12:21 +0000

    finished some TODOs and put the game icon in 'images' folder

--- Dominos/src/main.py
+++ Dominos/src/main.py
@@ -35,13 +35,12 @@ def generate_tiles():
         this function takes nothing and returns a complete dominos list of 28 elements
     each element of this list is a "tuple".
     """
-    # TODO make two loops instead of this mess :D
-    return [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),\
-            (0, 5), (0, 6), (1, 1), (1, 2), (1, 3),\
-            (1, 4), (1, 5), (1, 6), (2, 2), (2, 3),\
-            (2, 4), (2, 5), (2, 6), (3, 3), (3, 4),\
-            (3, 5), (3, 6), (4, 4), (4, 5), (4, 6),\
-            (5, 5), (5, 6), (6, 6)]
+	# TODO (DONE) make two loops instead of this mess :D
+    temp = []
+    for i in range(0,7):
+        for j in range(i,7):
+            temp.append((i,j))
+    return temp
 
 #----------------------------------------------------------------------------
 
@@ -63,11 +62,10 @@ def distribute_tiles(tiles_set, players_count):
     for i in range(0, 7):
         for j in range(players_count):
             random_number = randrange(0, len(tiles_set))
-            # TODO use the 'pop' method instead of the following active two lines
-            # i think the following commented line is gonna make it
-            # TODO final_list[j].append(tiles_set.pop[random_number])
-            final_list[j].append(tiles_set[random_number])
-            del(tiles_set[random_number])
+            # TODO (DONE) use the 'pop' method instead of the following active two lines            
+            final_list[j].append(tiles_set.pop(random_number))
+#            final_list[j].append(tiles_set[random_number])
+#            del(tiles_set[random_number])
 
     return final_list
 
@@ -134,10 +132,10 @@ def draw_tiles(screen):
         temp_tile.show_vertical()
 
     #Print the Computer Tiles (up-side-down)
-    # TODO changing the Computer tiles image
+    # TODO (DONE) changing the Computer tiles image
     for i in range(len(COMPUTER_TILES)):
         current_position = COMPUTER_TILES[i][1]
-        temp_tile = Tile(0, 0, screen, current_position[0], current_position[1])
+        temp_tile = Tile(9, 8, screen, current_position[0], current_position[1])
         temp_tile.show_vertical()
 
 #----------------------------------------------------------------------------
@@ -290,16 +288,34 @@ def END_GAME():
     pass
 
 #----------------------------------------------------------------------------
+def seticon(iconname):
+    """
+    give an iconname, a bitmap sized 32x32 pixels, black (0,0,0) will be alpha channel
+    
+    the windowicon will be set to the bitmap, but the black pixels will be full alpha channel
+     
+    can only be called once after pygame.init() and before somewindow = pygame.display.set_mode()
+    """
+    icon=pygame.Surface((32,32))
+    icon.set_colorkey((0,0,0))
+    rawicon=pygame.image.load(iconname)
+    for i in range(0,32):
+        for j in range(0,32):
+            icon.set_at((i,j), rawicon.get_at((i,j)))
+    pygame.display.set_icon(icon)
 
 if __name__ == '__main__':
 
-    #Initialize PyGame
+    #Initialize PyGame    
     pygame.init()
-
+    
+    # TODO (DONE) adding game icon
+    
+    seticon('images/icon.png')
+    
     #Create the Main Window
     screen = pygame.display.set_mode(main_window_resolution, 0, 32)
     pygame.display.set_caption("Dominos!")
-    # TODO adding game icon
 
     #initialize the game
     initialize(screen)
