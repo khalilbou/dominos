87f05aaf72dffa679d69bd15d21bb17e999ea92f
fci.noor <fci.noor@8686c424-25b9-11de-b186-ef1da62b6547>
2009-04-22 13:59:55 +0000

    modified the startwindow class

--- trunk/Dominos/src/main.py
+++ trunk/Dominos/src/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from pygame import time
 
 #------------------IMPORTS------------------------
 import pygame
--- trunk/Dominos/src/startwindow.py
+++ trunk/Dominos/src/startwindow.py
@@ -14,21 +14,32 @@ class splashWindow(object):
         seticon('images/icon.png')
         start_screen = pygame.display.set_mode((498,501),NOFRAME)
         init_background = pygame.image.load("images/init_bg.png").convert()
+        pygame.display.set_caption("Dominoes!")
+        onQuite_clicked_img = pygame.image.load("images/onclicked1.png")
+        onenjoy_clicked_img = pygame.image.load("images/onclicked2.png")
         ###
         #TODO: MAKE CENTER IS THE DEFAULT POSITION
         ###
         while True:
-            for event in pygame.event.get(): 
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()
+                     
                 if event.type == MOUSEBUTTONDOWN:
                     x,y = pygame.mouse.get_pos()
                     if x >= 172 and x <= 320 and y >= 435 and y<=480:
+                        start_screen.blit(onenjoy_clicked_img,(190,438))
+                        pygame.display.update()
+                        pygame.time.wait(500)
                         pass
                         ###
                         #TODO:INITIATE THE BOARD WINDOW
                         ###
-                    else:
-                        if x >= 324 and x <= 423 and y >= 327 and y<=369:
-                            exit()
+                    
+                    elif x >= 324 and x <= 423 and y >= 327 and y<=369:
+                        start_screen.blit(onQuite_clicked_img,(317,322))
+                        pygame.display.update()
+                        exit()
             
             start_screen.blit(init_background,(0,0))
             pygame.display.update()
